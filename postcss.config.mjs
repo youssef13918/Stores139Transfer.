4a9731bd06e96b2c2b7ba16ee530760d253ce8e0
Your Name <you@example.com>
2025-05-23 01:48:18 +0200

    inicial

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,12 @@
+import tailwindcss from '@tailwindcss/postcss';
+import autoprefixer from 'autoprefixer';
+
 /** @type {import('postcss-load-config').Config} */
 const config = {
-  plugins: {
-    tailwindcss: {},
-  },
+  plugins: [
+    tailwindcss(),
+    autoprefixer(),
+  ],
 };
 
 export default config;
