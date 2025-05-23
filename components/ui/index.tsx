18931b0f31b75eb580aa5ea344bfc5e18a8a0fa3
youssef13918 <fmelusa@gmail.com>
2025-05-23 19:27:39 +0200

    Update index.tsx

--- components/ui/index.tsx
+++ components/ui/index.tsx
@@ -1,11 +1,15 @@
-'use client'
-
-import { MiniKitProvider } from '@worldcoin/minikit-js/minikit-provider'
-
-export function MinikitProviderWrapper({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return <MiniKitProvider>{children}</MiniKitProvider>
+import { MiniKitProvider } from '@worldcoin/minikit-js/minikit-provider';
+export default async function Root({
+	children,
+}: Readonly<{
+	children: React.ReactNode
+}>) {
+	return (
+		<html lang="en">
+			<MiniKitProvider>
+				<body className={inter.className}>{children}</body>
+			</MiniKitProvider>
+		</html>
+	)
 }
+
