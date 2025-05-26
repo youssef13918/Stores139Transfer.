7fe1b3b05f9c21bd22c1dcf9a7320816f092eacd
Your Name <you@example.com>
2025-05-26 17:12:44 +0200

    lo

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es">
-      <body className={inter.className}>
+      <body>
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
           <AuthProvider>
             {children}
